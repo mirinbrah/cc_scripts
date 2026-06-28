7de6297c0fb5a56e489ceb4db24a5d44655351e6
Mikhail Ryabchikov <Nolifegonnamad@gmail.com>
2026-06-28 10:30:20 +0400

    pass

---
+++ password.lua
@@ -0,0 +1,25 @@
+password = "22307"
+side = "back"
+
+while true do
+  term.clear()
+  term.setCursorPos(1, 1)
+
+  print("Enter password:")
+  input = read("*")
+
+  if input == password then
+    print("Correct password!")
+    print("Redstone ON")
+
+    redstone.setOutput(side, true)
+    sleep(5)
+    redstone.setOutput(side, false)
+
+    print("Redstone OFF")
+    sleep(2)
+  else
+    print("Wrong password")
+    sleep(2)
+  end
+end
